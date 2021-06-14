128811de54381764d4052541d563f874b60b059d
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-06-14 23:11:57 +0700

    Deploy into docker

--- services/query/index.js
+++ services/query/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { randomBytes } = require("crypto");
 const axios = require("axios");
 const cors = require("cors");
 
@@ -48,7 +47,7 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log("Listening on 4002");
 
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get("http://event-bus-srv:4005/events");
   for (let event of res.data) {
     handleEvent(event.type, event.data);
   }
